a11dbb0c3e0feb97da510a6015ac7cf7ecabd703
frankzyj2000 <48901610+frankzyj2000@users.noreply.github.com>
2019-04-23 12:09:47 -0400

    Upgraded to .Net framework 4.6 and Visual Studio 2015

--- Models/GenuinaDB.Designer.cs
+++ Models/GenuinaDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\GenuinaBI\Main\Src\GenuinaBI\GenuinaBI\Models\GenuinaDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\fzhang\Documents\GitHub\GenuaBI\Models\GenuinaDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
